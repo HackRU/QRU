171e264e489444f0e122820fcf74852e91e18979
Qasim Abbas <qasimabbas@nbp-160-20.nbp.ruw.rutgers.edu>
2017-10-12 03:23:26 -0400

    Everything is up and running

--- app/src/providers/label-printer.ts
+++ app/src/providers/label-printer.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { AlertController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -24,16 +24,30 @@ export class LabelPrinter {
 
   print(firstName: String, lastName: String, email: String) {
     this.http.get(this.baseUrl + '/auth?pass=' + this.pass)
-      .toPromise().then((token) => {
-        this.http.get(this.baseUrl + '/print?csrf=' + token + '&first_name=' +
-          firstName + '&last_name=' + lastName + '&email=' + email);
+      .map(token => token).subscribe((token) => {
+        var fullUrl = this.baseUrl + '/print?csrf=' + token.text() + '&first_name=' +
+              firstName + '&last_name=' + lastName + '&email=' + email;
+        this.http.get(fullUrl).toPromise().then((reply) => {
+            this.alertCtrl.create({
+                title: 'reply from print',
+                subTitle: reply.text(),
+                buttons: ['OK']
+            }).present();
+        });
+        this.alertCtrl.create({
+            title: 'called auth',
+            subTitle: fullUrl,
+            buttons: ['OK']
+        }).present();
+/*
       }).catch((error) => {
         console.error(error);
         this.alertCtrl.create({
           title: error,
           subTitle: 'failed to print label',
           buttons: ['OK']
-        });
+        }).present();
+*/
       });
   }
 
